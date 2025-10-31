fbfe645ade7ffcc0d6636e8d1445a641667a8866
zain-khokhar <zaink334705@gmail.com>
2025-10-31 18:21:25 +0500

    Remove unnecessary echo logic from pdfsHandler

--- src/utils/courseCode.js
+++ src/utils/courseCode.js
@@ -1,9 +1,24 @@
 // List of all valid course codes as a Set for fast lookup
 const courseCodes = new Set([
-	'cs101', 'cs201', 'cs202', 'cs301', 'cs302', 'cs304', 'cs311',
-	'cs401', 'cs403', 'cs501', 'cs502', 'cs506', 'cs507', 'cs508',
-	'cs510', 'cs601', 'cs602', 'cs603', 'cs604', 'cs605', 'cs606',
-	'cs609', 'cs610', 'cs614', 'cs615', 'cs619'
+	'cs101', 'cs201', 'cs202', 'cs205', 'cs206', 'cs301', 'cs302', 'cs304', 'cs306', 'cs310', 'cs311', 'cs312', 'cs314', 'cs315', 'cs401', 'cs402', 'cs403', 'cs405', 'cs406', 'cs407', 'cs408', 'cs409', 'cs410', 'cs411', 'cs420', 'cs431', 'cs432', 'cs435', 'cs441', 'cs442', 'cs501', 'cs502', 'cs504', 'cs505', 'cs506', 'cs507', 'cs508', 'cs510', 'cs511', 'cs512', 'cs513', 'cs515', 'cs519', 'cs521', 'cs525', 'cs601', 'cs602', 'cs603', 'cs604', 'cs605', 'cs606', 'cs607', 'cs608', 'cs609', 'cs610', 'cs611', 'cs614', 'cs615', 'cs619', 'cs620', 'cs621', 'cs626', 'cs627', 'cs628', 'cs630', 'cs631', 'cs701', 'cs702', 'cs703', 'cs704', 'cs706', 'cs707', 'cs708', 'cs709', 'cs710', 'cs711', 'cs712', 'cs713', 'cs716', 'cs718', 'cs719', 'cs720', 'cs721', 'cs723', 'cs724', 'cs725', 'cs726'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 ]);
 
 module.exports = courseCodes;
