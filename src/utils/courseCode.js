6346007eec822454f5151cb68d4077c0d3275b76
zain-khokhar <zaink334705@gmail.com>
2025-10-31 18:57:46 +0500

    Optimize course code detection using Set and word matching

--- src/utils/courseCode.js
+++ src/utils/courseCode.js
@@ -6,9 +6,10 @@ const courseCodes = new Set([
     'edu101', 'edu201', 'edu301', 'edu302', 'edu303', 'edu304', 'edu305', 'edu401', 'edu402', 'edu403', 'edu405', 'edu406', 'edu407', 'edu410', 'edu411', 'edu430', 'edu431', 'edu433', 'edu501', 'edu510', 'edu512', 'edu515', 'edu516', 'edu601', 'edu602', 'edu604', 'edu630', 'edu654',
     'mcm101', 'mcm301', 'mcm304', 'mcm310', 'mcm311', 'mcm401', 'mcm411', 'mcm431', 'mcm501', 'mcm511', 'mcm512', 'mcm514', 'mcm515', 'mcm516', 'mcm517', 'mcm520', 'mcm531', 'mcm532', 'mcm601', 'mcm604', 'mcm610', 'mcm611', 'mcm619', 'mcm499',
     'eco302', 'eco303', 'eco401', 'eco402', 'eco403', 'eco404', 'eco499', 'eco501', 'eco601', 'eco603', 'eco604', 'eco605', 'eco606', 'eco607', 'eco608', 'eco609', 'eco610', 'eco612', 'eco613', 'eco614', 'eco615', 'eco616', 'eco619', 'eco622',
-     'eng001', 'eng101', 'eng201', 'eng301', 'eng401', 'eng498', 'eng499', 'eng501', 'eng502', 'eng503', 'eng504', 'eng505', 'eng506', 'eng507', 'eng508', 'eng509', 'eng510', 'eng511', 'eng512', 'eng513', 'eng514', 'eng515', 'eng516', 'eng517', 'eng518', 'eng519', 'eng520', 'eng521', 'eng522', 'eng523', 'eng524', 'eng527', 'eng529',
-     'acc101', 'acc311', 'acc501',
-     
+    'eng001', 'eng101', 'eng201', 'eng301', 'eng401', 'eng498', 'eng499', 'eng501', 'eng502', 'eng503', 'eng504', 'eng505', 'eng506', 'eng507', 'eng508', 'eng509', 'eng510', 'eng511', 'eng512', 'eng513', 'eng514', 'eng515', 'eng516', 'eng517', 'eng518', 'eng519', 'eng520', 'eng521', 'eng522', 'eng523', 'eng524', 'eng527', 'eng529',
+    'acc101', 'acc311', 'acc501',
+    'sta100', 'sta301', 'sta408', 'sta5102', 'sta620', 'sta621', 'sta630', 'sta631', 'sta632', 'sta641', 'sta642', 'sta643', 'sta644', 'sta730',
+    
 
 
 ]);
